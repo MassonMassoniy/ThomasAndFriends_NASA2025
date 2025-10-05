3ba73b62d186622aab76750ff95be69c8ca3463c
drmydrmy <vms05new@ya.ru>
2025-10-05 16:00:14 -0500

    Update page.tsx

--- frontend/src/app/(admin)/page.tsx
+++ frontend/src/app/(admin)/page.tsx
@@ -15,8 +15,7 @@ import WeatherMetricsConfigurator from "@/components/weather/WeatherMetricsConfi
 
 export const metadata: Metadata = {
   title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Home for TailAdmin Dashboard Template",
+    "Will It Rain on My Parade - NASA App Challenge"
 };
 
 const exampleData = {
